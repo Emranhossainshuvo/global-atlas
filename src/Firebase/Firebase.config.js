b0a1dc8ba5f62b86138b97cd3a11b22a87d2e7dd
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-03 21:47:38 +0600

    added firebase SDK

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTuDxKHog5WG1MuBcg6GG41CpSu_LhG8k",
+  authDomain: "global-atlas.firebaseapp.com",
+  projectId: "global-atlas",
+  storageBucket: "global-atlas.appspot.com",
+  messagingSenderId: "955771012805",
+  appId: "1:955771012805:web:abf5070e4d2615fd2d52ac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app; 
